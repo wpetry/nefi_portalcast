03407d4115af6fb75c40fb9b7a4ba1c920407e76
melaniecolon <53380629+melaniecolon@users.noreply.github.com>
2019-07-31 13:20:28 -0500

    Update model to include temperature

--- pen_random_walk.R
+++ pen_random_walk.R
@@ -20,29 +20,44 @@ library(portalr)
 library(dplyr)
 library(remotes)
 
-df=abundance()
-str(df)
+df.a=abundance(time="newmoon", clean=FALSE)
+temp=weather(level="newmoon", fill=TRUE)
+temp=subset(temp, !newmoonnumber=="NA")
+temp=subset(temp, !newmoonnumber>519)
+df.a=subset(df.a, !newmoonnumber<32)
 
-##FILL IN EMPTY PERIODS##
-print(df$period)
-475-27#max period-min period
-448-416#number of missing periods
-df2<-data.frame(period=min(df$period):max(df$period))
+str(df.a)
+str(temp)
+
+max(df.a$newmoonnumber)
+max(temp$newmoonnumber)
+min(df.a$newmoonnumber)
+min(temp$newmoonnumber)
+519-32
+
+df<-merge(df.a, temp, by="newmoonnumber", all=TRUE)
+str(df)
+head(df)
+df2<-subset(df, select=c("newmoonnumber","PP","mintemp"))
 str(df2)
 
-df3<-merge(df, df2, by="period", all=TRUE)
+##FILL IN EMPTY PERIODS##
+
+df3<-data.frame(newmoonnumber=min(df$newmoonnumber):max(df$newmoonnumber))
 str(df3)
 
-df4<-subset(df3, select=c("period","PP"))
+df4<-merge(df2, df3, by="newmoonnumber", all=TRUE) 
 str(df4)
 
-ggplot(df4, aes(x=period, y=PP))+geom_point()+geom_line()
+dev.off()
+ggplot(df4, aes(x=newmoonnumber, y=PP))+geom_point()+geom_line()
 
 y = df4$PP
 y2=df4$PP
-period=df4$period
+newmoonnumber=df4$newmoonnumber
 y[floor(length(y)-0.25*(length(y))):length(y)]<-NA
 y
+temp=df4$mintemp
 
 
 RandomWalk = "
@@ -50,7 +65,8 @@ model{
   
 #### Process Model
   for(t in 2:n){
-    x[t]~dnorm(x[t-1],tau_add)
+    mu[t] <- x[t-1] + betaIntercept + betaTmin*temp[t-1]
+    x[t]~dnorm(mu[t],tau_add)
   }
   
   #### Data Model
@@ -61,10 +77,12 @@ model{
   #### Priors
   x[1] ~ dnorm(x_ic,tau_ic)
   tau_add ~ dgamma(a_add,r_add)
+  betaIntercept~dnorm(0,0.001)
+  betaTmin~dnorm(0,0.001)  
 }
 "
 
-data <- list(y=y,n=length(y),x_ic=100 ,tau_ic=100,a_add=1,r_add=1)
+data <- list(y=y,n=length(y),temp=temp, x_ic=100 ,tau_ic=100,a_add=1,r_add=1)
 
 nchain = 3
 init <- list()
@@ -88,19 +106,20 @@ jags.out   <- coda.samples (model = j.model,
                             variable.names = c("x","tau_add"),
                             n.iter = 10000)
 
-period.rng = c(1,length(period)) ## adjust to zoom in and out
+newmoon.rng = c(1,length(newmoonnumber)) ## adjust to zoom in and out
 out <- as.matrix(jags.out)
 x.cols <- grep("^x",colnames(out)) ## grab all columns that start with the letter x
 ci <- apply(out[,x.cols],2,quantile,c(0.025,0.5,0.975)) 
 str(ci)
 
-plot(period,ci[2,],type='n',ylim=range(y,na.rm=TRUE),ylab="Abundance",xlim=period[period.rng])
+par(mar=c(1,1,1,1))
+plot(newmoonnumber,ci[2,],type='n',ylim=range(y,na.rm=TRUE),ylab="Abundance",xlim=newmoonnumber[newmoon.rng])
 
 ## adjust x-axis label to be monthly if zoomed
 
-ecoforecastR::ciEnvelope(period,ci[1,],ci[3,],col=ecoforecastR::col.alpha("lightBlue",0.75))
-points(period,y,pch="+",cex=1.5)
-points(period,y2,pch="+",cex=0.5)
+ecoforecastR::ciEnvelope(newmoonnumber,ci[1,],ci[3,],col=ecoforecastR::col.alpha("lightBlue",0.75))
+points(newmoonnumber,y,pch="+",cex=1.5)
+points(newmoonnumber,y2,pch="+",cex=0.5)
 
 hist(1/sqrt(out[,1]),main=colnames(out)[1])
 hist(1/sqrt(out[,2]),main=colnames(out)[2])
